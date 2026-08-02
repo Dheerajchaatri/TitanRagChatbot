6f21b91745ee03a620dd2e2943bdd2c748673ac9
Dheeraj Kumar <120976659+Dheerajchaatri@users.noreply.github.com>
2026-08-02 19:40:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import streamlit as st
 
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 
