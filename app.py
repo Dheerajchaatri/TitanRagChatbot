5a9d18c1baeb0bf7c9d3fc2f1fadb24d312384f5
Dheeraj Kumar <120976659+Dheerajchaatri@users.noreply.github.com>
2026-08-02 18:42:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-
-# --- CORRECT LANGCHAIN IMPORTS ---
-from langchain.chains import (
-    create_history_aware_retriever,
-    create_retrieval_chain,
-)
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-
 from langchain_core.messages import HumanMessage, AIMessage
 
 # --- PAGE CONFIGURATION ---
