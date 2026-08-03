b8a52fac104be18500ad835a905fc87195e8bbc8
Dheeraj Kumar <120976659+Dheerajchaatri@users.noreply.github.com>
2026-08-03 18:35:32 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,7 @@ from langchain_text_splitters import (
     RecursiveCharacterTextSplitter
 )
 
-from langchain_community.vectorstores import (
-    Chroma
-)
+from langchain_chroma import Chroma
 
 from langchain_huggingface import (
     HuggingFaceEmbeddings
